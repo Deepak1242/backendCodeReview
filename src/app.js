26a3d06b0506c6b9e1c7cb2fa25b0e3ed6840a0a
Deepak1242 <bhagatdeepak1242@gmail.com>
2025-03-31 13:53:09 +0530

    Added CORS, Helmet, and static file serving

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
 
-app.use(cors())
+app.use(cors());  
+
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://backendcodereview.onrender.com"],
+        styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "data:", "https://backendcodereview.onrender.com"],
+        imgSrc: ["'self'", "data:"],
+        connectSrc: ["'self'", "https://backendcodereview.onrender.com"],
+      },
+    },
+  }))
 
 
 app.use(express.json())
